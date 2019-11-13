9c70ad55796a4b812ce3b77183e50dbf42dffd04
CelestinJacobs <33254800+CelestinJacobs@users.noreply.github.com>
2019-11-13 22:22:49 +0000

    Initial commit

---
+++ Source/FirstMMAlgorithm/FirstMMAlgorithmGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstMMAlgorithmGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTMMALGORITHM_API AFirstMMAlgorithmGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/FirstMMAlgorithm/Matchmaker.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Matchmaker.h"
+
+Matchmaker::Matchmaker()
+{
+}
+
+Matchmaker::~Matchmaker()
+{
+}
